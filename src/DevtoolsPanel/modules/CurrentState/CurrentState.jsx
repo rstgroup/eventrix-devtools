ce50559acb1ab472c07b1c1d00103ac59a09f45a
Mariusz Przodała <mariuszprzodala@Mariuszs-MacBook-Pro-8.local>
2023-09-27 13:20:58 +0200

    state preview sort

--- src/DevtoolsPanel/modules/CurrentState/CurrentState.jsx
+++ src/DevtoolsPanel/modules/CurrentState/CurrentState.jsx
@@ -1,21 +1,22 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useMemo} from 'react';
 import { useEmit, useEventrixState } from 'eventrix';
 import { ObjectInspector } from "react-inspector";
 import {Stack} from "@mui/material";
 
 import {STATE_FETCH} from "../../events";
+import {sortState} from "./helpers";
 
 const CurrentState = () => {
     const emit = useEmit();
     const [currentState = []] = useEventrixState('currentState');
     const fetchState = useCallback(() => { emit(STATE_FETCH) }, [emit]);
-
+    const alphabeticallySortedCurrentState = useMemo(() => sortState(currentState), [currentState])
     useEffect(() => {
         fetchState()
     }, [fetchState]);
     return (
         <Stack width="100%" >
-            <ObjectInspector data={currentState} />
+            <ObjectInspector data={alphabeticallySortedCurrentState} />
         </Stack>
     )
 };
